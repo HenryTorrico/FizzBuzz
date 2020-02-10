fe206b93cdc1d4b8343e7913d401be09c083929b
Henry Torrico <guillerbauti@gmail.com>
2020-02-10 15:23:11 -0400

    refactoring again

--- src/Program.java
+++ src/Program.java
@@ -1,25 +1,19 @@
 public class Program {
+	
+	
 	public String main(int numero)
 	{
-		String resp;
-		resp = "";
 		if (numero % 3 != 0 && numero % 5 != 0)
 		{
-			resp=Integer.toString(numero);                     
+			return Integer.toString(numero);                     
 		}
-		else
-		{
-			if(numero % 3 == 0)
-			{
-				resp= resp + "Fizz";
-			} 
-			if(numero % 5 == 0)
-			{
-				resp = resp + "Buzz";
-			}
-		}
-		return resp;
-	    
+		else if (numero % 15 == 0) {
+            return "FizzBuzz";
+        } else if (numero % 5 == 0) {
+            return "Buzz";
+        }
+        return "Fizz";
+ 
 	}		
 }
 
